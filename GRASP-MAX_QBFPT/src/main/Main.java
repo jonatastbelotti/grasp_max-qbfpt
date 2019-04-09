77db08ff078cd276c09dd408af7d6ed2ebdad578
felipe <fcpereira@dcomp.ufs.br>
2019-04-08 22:43:08 -0300

    Aumento numero limite de iterações para 1000

--- GRASP-MAX_QBFPT/src/main/Main.java
+++ GRASP-MAX_QBFPT/src/main/Main.java
@@ -24,7 +24,7 @@ public class Main {
     public static int CONSTRUCTION_MECHANISM;
     public static boolean FIRST_IMPROVING;
     public static int TIME_LIMIT = 30; // In minutes
-    public static int ITERATIONS_LIMIT = 100; // Iterations without improvement in incumbent (negative values for not using iterations limit)
+    public static int ITERATIONS_LIMIT = 1000; // Iterations without improvement in incumbent (negative values for not using iterations limit)
     public static String outputCsv;
     
     public static final String[] FILES_LIST = new String[]{
